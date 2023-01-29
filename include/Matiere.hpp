10aec5a7d2bf6ff969d8ee1c9acf9f02780c58c5
iMoubarak <imoubarak862@gmail.com>
2023-01-29 14:45:53 +0100

    fix (TerminalC.cpp) : tentative d'accès à une variable privee

--- include/Matiere.hpp
+++ include/Matiere.hpp
@@ -3,17 +3,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
-    class Matiere
-    {
-        string nom;
-        float note;
-        int coef;
-        public:
-            string GetNom();
-            float GetNote();
-            int GetCoef();
-            void SetNom(string);
-            void SetNote(float);
-            void SetCoef(int);
-    };
+class Matiere
+{
+    string nom;
+    float note;
+    int coef;
+    public:
+        string GetNom();
+        float GetNote();
+        int GetCoef();
+        void SetNom(string);
+        void SetNote(float);
+        void SetCoef(int);
+};
 #endif
--- include/TerminaleC.hpp
+++ include/TerminaleC.hpp
@@ -1,13 +1,13 @@
 #ifndef TERMINALEC_HPP
 #define TERMINALEC_HPP
 #include "Terminale.hpp"
-  class TerminaleC:Terminale
-  {
+class TerminaleC:Terminale
+{
   	public:
   		TerminaleC();
   		void SaisirNote();
   		float CalculerMoyenne();
-  };
+};
 #endif
 
 
