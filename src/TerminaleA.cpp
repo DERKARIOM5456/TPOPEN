86dc68482e1bb870d5e26555ee11b379fe9ce909
AlaziI <alaziissoufoudodo@gmail.com>
2023-01-30 09:51:30 +0100

    ci (TerminaleA.cpp): finalisation de la classe Terminale A

---
+++ src/TerminaleA.cpp
@@ -0,0 +1,11 @@
+#ifndef TERMINALEA_HPP_
+#define TERMINALEA_HPP_
+#include "Terminale.hpp"
+class TerminaleD: public Terminale
+{
+   float CalculerMoyenne();
+   public:
+   TerminaleA();
+   void SaisirNote();
+};
+#endif
